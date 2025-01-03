adc0e9ae3fa472ddc345a1b028c81b3f8ac896bc
enteresanlikk <zeki1198@gmail.com>
2025-01-03 21:52:41 +0300

    Add github action.

--- pm2.sh
+++ pm2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-PM2_APP_NAME='lumodine-qr'
+PM2_APP_NAME='lumodine-qr-api'
 
 if pm2 list | grep "online" | grep $PM2_APP_NAME; then
     PM2_EXIST="Yes"
 else
     PM2_EXIST="No"
 fi
-
-killall -r firefox-bin
 
 if [ "$PM2_EXIST" = "Yes" ]; then
     pm2 restart $PM2_APP_NAME
